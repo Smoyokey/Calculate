c5f77d793db345cd29fe8ff3d6aa7dbf81f4d812
Smoyokey <oleg37306@gmail.com>
2024-01-03 21:36:00 +0300

    Add testing aplication

---
+++ app/src/androidTest/java/com/example/calculate/TestUi.kt
@@ -0,0 +1,82 @@
+package com.example.calculate
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.example.calculate.model.Action
+import com.example.calculate.model.CalculateUiState
+import com.example.calculate.ui.CalculateApp
+import com.example.calculate.ui.toRes
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Деление на 0
+ * Кнопка очистить
+ */
+
+class TestUi {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val numberOne = "20"
+    private val numberTwo = "5"
+
+    @Before
+    fun createScreen() {
+        composeTestRule.setContent {
+            CalculateApp()
+        }
+    }
+
+    @Test
+    fun testAction_oneSumOne_result() {
+        val action = "+"
+        startTest(action, "25")
+    }
+
+    @Test
+    fun testAction_oneSubtractionOne_result() {
+        val action = "-"
+        startTest(action, "15")
+    }
+
+    @Test
+    fun testAction_oneMultiplicationOne_result() {
+        val action = "*"
+        startTest(action, "100")
+    }
+
+    @Test
+    fun testAction_oneDivisionOne_result() {
+        val action = "/"
+        startTest(action, "4")
+    }
+
+    @Test
+    fun testAction_buttonReset_click() {
+        val action = "*"
+        startTest(action, "100")
+
+        composeTestRule.onNodeWithStringId(R.string.reset).performClick()
+
+        composeTestRule.onNodeWithText(" + =").assertIsDisplayed()
+    }
+
+    private fun startTest(action: String, res: String) {
+        composeTestRule.onNodeWithStringId(R.string.writeNumberOne).performTextInput(numberOne)
+        composeTestRule.onNodeWithStringId(R.string.writeNumberTwo).performTextInput(numberTwo)
+        composeTestRule.onNodeWithText(action).performClick()
+
+        composeTestRule
+            .onNodeWithText("$numberOne $action $numberTwo = $res")
+            .assertIsDisplayed()
+    }
+}
